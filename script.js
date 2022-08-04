32be439acd928b1215fc6964d346b59ac1ffb904
rola75 <cloudgit.241c2@simplelogin.co>
2022-08-04 16:19:34 -0500

    click listener added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//
+//The first thing I need is a boad for tic tac toe.
+// After I need to be able to put X in all the blocks on click
+
+
+const blocks = document.querySelectorAll(".blocks");
+//console.log(blocks);
+
+
+
+
+const blockDisplayer = blocks.forEach((block) => {
+    block.addEventListener("click", () =>{
+        console.log("click is working");
+    } );
+})
