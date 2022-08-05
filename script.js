e3a1f0dcbcdbef607425b07e9d79a49663d287f7
rola75 <cloudgit.241c2@simplelogin.co>
2022-08-05 10:34:57 -0500

    x and o can be displayed on the screen

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //The first thing I need is a boad for tic tac toe.
 // After I need to be able to put X in all the blocks on click
+            //x is on the screen
+//how to get o on the screen
+            //o is on the screen
+
 
 
 const blocks = document.querySelectorAll(".section_button_block");
@@ -7,11 +11,30 @@ const blocks = document.querySelectorAll(".section_button_block");
 
 
 const handleBlockClicker = (event) => {
-    const element = event.target;
-    console.log("checking the handler click");
+    const blockText = event.target;
+    //console.log("checking the handler click");
+    let letters = blockText.innerText;
+    let playerTurn = true;
+    
+    if (letters != "") {
+        return;
+    } else {
+        let letters = "";
+        if(playerTurn) {      //if playerTurn is false O appears 
+            letters = "X";
+        } else {
+            letters ="O";
+        }
+        console.log(letters)
 
-    const letterInput = element.innerText;
-    console.log(letterInput);
+
+        blockText.innerText = letters;
+        playerTurn = !playerTurn
+        
+       //console.log("here is letterinput" + letterInput)
+        console.log("letters" + letters);
+    }
+    
 
     };
 
